c1be225d21a0ea7a8f2f02206d67181522bbc076
Kalle Hirvola <kalle@rareagency.fi>
2021-02-01 16:32:05 +0200

    Show basic game canvas

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.scss";
+import GameCanvas from "./components/GameCanvas";
+
+function App() {
+  return (
+    <div className="root">
+      <GameCanvas />
+    </div>
+  );
+}
+
+export default App;
