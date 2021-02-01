9ef58b613d3708fd514472c8e2e5908cd15dbdc9
Kalle Hirvola <khirvola@gmail.com>
2021-02-01 16:32:05 +0200

    Show basic game canvas

---
+++ src/components/GameCanvas/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.scss";
+
+function GameCanvas() {
+  return <canvas className="game-canvas" width={735} height={375} />;
+}
+
+export default GameCanvas;
