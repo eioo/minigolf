073f8cfacc4d5963f525189068ab2c682d2e74e1
naftis <pyry.rouvila@gmail.com>
2021-02-01 19:43:48 +0200

    Add routing and basic frontpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from "react";
-import "./App.scss";
-import GameCanvas from "./components/GameCanvas";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./views/Home";
+import Game from "./views/Game";
+import Lobby from "./views/Lobby";
+
+import "./styles/styles.scss";
 
 function App() {
   return (
-    <div className="root">
-      <GameCanvas />
+    <div className="App-container">
+      <div id="game">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="lobby/*" element={<Lobby />} />
+            <Route path="game/*" element={<Game />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
