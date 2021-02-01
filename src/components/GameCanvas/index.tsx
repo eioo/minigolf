19072dd59cae2aaf7ac2f5d3bcb52d37f2f96b6d
Kalle Hirvola <khirvola@gmail.com>
2021-02-01 18:12:57 +0200

    Add function for loading spritesheets

--- src/components/GameCanvas/index.tsx
+++ src/components/GameCanvas/index.tsx
@@ -1,8 +1,33 @@
-import React from "react";
+import * as PIXI from "pixi.js";
+import React, { useEffect, useRef, useState } from "react";
+import { gameLoop } from "../../game";
 import "./styles.scss";
 
 function GameCanvas() {
-  return <canvas className="game-canvas" width={735} height={375} />;
+  const [app, setApp] = useState<PIXI.Application>();
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    if (!canvasRef.current) {
+      return;
+    }
+    const app = new PIXI.Application({
+      height: 375,
+      width: 735,
+      view: canvasRef.current,
+      backgroundColor: 0x16aa16,
+    });
+    gameLoop(app);
+    setApp(app);
+
+    return () => {
+      app.stop();
+    };
+  }, [canvasRef]);
+
+  return (
+    <canvas ref={canvasRef} className="game-canvas" width={735} height={375} />
+  );
 }
 
 export default GameCanvas;
