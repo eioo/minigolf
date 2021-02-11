c54afb96f6e1426e0af073006379f1999c94c5bd
Kalle Hirvola <khirvola@gmail.com>
2021-02-11 19:20:59 +0200

    Replace react-router-dom with wouter to fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./views/Home";
-import Game from "./views/Game";
-import Lobby from "./views/Lobby";
-
+import { Route } from "wouter";
 import "./styles/styles.scss";
+import Game from "./views/Game";
+import Home from "./views/Home";
+import Lobby from "./views/Lobby";
 
 function App() {
   return (
     <div className="App-container">
       <div id="game">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="lobby/*" element={<Lobby />} />
-            <Route path="game/*" element={<Game />} />
-          </Routes>
-        </BrowserRouter>
+        <Route path="/" component={Home} />
+        <Route path="/lobby/:gameMode" component={Lobby} />
+        <Route path="/game/:gameId" component={Game} />
       </div>
     </div>
   );
--- src/components/GameModeSelector/GameModeSelector.tsx
+++ src/components/GameModeSelector/GameModeSelector.tsx
@@ -1,40 +1,34 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Link } from "wouter";
 import styles from "./GameModeSelector.module.scss";
 
 export const GameModeSelector = () => {
-  const navigate = useNavigate();
-
-  const handleClick = () => {
-    navigate;
-  };
-
   return (
     <div className={styles.container}>
       <div className={styles.gameMode}>
         <header>Single player</header>
         <div className={styles.buttonContainer}>
-          <button onClick={() => navigate("/game/1")} className={styles.button}>
-            Single player
-          </button>
+          <Link href="/game/1">
+            <button className={styles.button}>Single player</button>
+          </Link>
         </div>
       </div>
 
       <div className={styles.gameMode}>
         <header>Dual player</header>
         <div className={styles.buttonContainer}>
-          <button onClick={() => navigate("/game/1")} className={styles.button}>
-            Dual player
-          </button>
+          <Link href="/game/1">
+            <button className={styles.button}>Dual player</button>
+          </Link>
         </div>
       </div>
 
       <div className={styles.gameMode}>
         <header>Multiplayer</header>
         <div className={styles.buttonContainer}>
-          <button onClick={() => navigate("/game/1")} className={styles.button}>
-            Multiplayer
-          </button>
+          <Link href="/game/1">
+            <button className={styles.button}>Multiplayer</button>
+          </Link>
         </div>
       </div>
     </div>
