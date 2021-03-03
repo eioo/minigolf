f9a711df2041d1d74c6e29bec7b0793dc7915cee
Kalle Hirvola <khirvola@gmail.com>
2021-03-03 18:27:44 +0200

    Add linting (ESLint, prettier)
    Fix broken imports

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  rules: {},
+};
