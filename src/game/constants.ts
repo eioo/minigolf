76719a88c9bf5b8c57410ea8eac9747cdce88f8e
Nokkasiili <46995454+Nokkasiili@users.noreply.github.com>
2021-03-25 17:17:59 +0200

    point.ts: initial

--- src/game/constants.ts
+++ src/game/constants.ts
@@ -3,5 +3,6 @@ export const HALF_BALL = 6.5;
 export const GAME_WIDTH = 735;
 export const GAME_HEIGHT = 375;
 export const TILE_SIZE = 15;
+export const HALF_TILE = 7.5;
 export const MAP_WIDTH = 49;
 export const MAP_HEIGHT = 25;
---
+++ src/game/point.ts
@@ -0,0 +1,30 @@
+import { HALF_TILE, TILE_SIZE } from './constants';
+
+class Point {
+  public x: number;
+  public y: number;
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+  add(other: Point): Point {
+    return new Point(this.x + other.x, this.y + other.y);
+  }
+  sub(other: Point): Point {
+    return new Point(this.x - other.x, this.y - other.y);
+  }
+  div(other: Point): Point {
+    return new Point(this.x / other.x, this.y / other.y);
+  }
+  mul(other: Point): Point {
+    return new Point(this.x * other.x, this.y * other.y);
+  }
+  to_screen(): Point {
+    return new Point(this.x * TILE_SIZE + HALF_TILE, this.y * TILE_SIZE + HALF_TILE);
+  }
+  to_map(): Point {
+    return new Point(this.x / TILE_SIZE, this.y / TILE_SIZE);
+  }
+}
+
+export default Point;
