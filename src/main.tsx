71c71d4dc9c68604efe92f8e18198aca7f88e380
eioo <khirvola@gmail.com>
2023-05-10 01:15:09 +0300

    Upgrade packages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('root');
+
+if (container) {
+  const root = createRoot(container);
+
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+}
